d0534bc3e69a926e7f08304bd823a49c07a129ae
jawwad-ali <alijawwad001@gmail.com>
2020-10-29 16:02:52 +0500

    start making react compoennts

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React, { Component } from 'react';
-import Footer from './component/Footer';
+// import Footer from './component/Footer';
 import Header from "./component/Header"
+import Content from "./component/Content"
 
 class App extends Component {
-  constructor() {
-    super()
-    // this.state = {
-    //   title: "Learn react",
-    //   version: "0.0.1"
-    // }
-
-    // function MyFunction () {
-    //   alert("hello")
-    // }
-    // MyFunction()
-  }
-
-
   render() {
-    const myFun = (num1, num2) => {
-      const add = num1 + num2
-      console.log(add)
-    }
-    myFun()
     return (
       <div>
         <Header />
-
-        <p>THis is sentence</p>
-        <button onClick={myFun(10, 2)} >Click here</button>
-
-        <Footer />
+        <Content />
+        {/* <Footer /> */}
       </div>
     )
   }
--- src/component/Header.js
+++ src/component/Header.js
@@ -4,7 +4,22 @@ import "./Header.css"
 export default function Header() {
     return (
         <div>
-            <h1 style={{ color:"brown" , background:"pink" }}>This is my Header</h1>
+            <header>
+                <div className="container-fluid px-0">
+                    <div className="row no-gutters">
+                        <div className="col-lg-6">
+                            <a className="website-logo" href="#">My first React Website</a>
+                        </div>
+                        <div className="col-lg-6 navbar-links">
+                            <ul className="navbar-links-ul">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Contact</a></li>
+                                <li><a href="#">About</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </header>
         </div>
     )
 }
