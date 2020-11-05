ba8abd8dbab2e3544ccea191987be1ac8012634d
jawwad-ali <alijawwad001@gmail.com>
2020-11-05 15:58:16 +0500

    array and obj destructuring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from 'react';
-import Header from "./component/Header"
-import Content from "./component/Content"
-import Card from "./component/Card"
-import Footer from './component/Footer';
+// import Header from "./component/Header"
+// import Content from "./component/Content"
+// import Card from "./component/Card"
+// import Footer from './component/Footer';
+
+// let arr = ["ali", "asad" , "cricket" , "biryani"]
+// let [myname, friend, fsport] = arr
+// let [myname, , ,  fdish] = arr
+
+// SPREAD OPERATOR
+// let basic = ["ali", "19"]
+// let prgrmng = ["html", "css", "js" , "react"]
+
+// let merge = [basic, ...prgrmng]
+
+let obj = {
+  username : "Micheal",
+  website : "micheal.com"
+}
+
+let {username , website} = obj
 
 class App extends Component {
+
+
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header />
         <Content />
         <Card />
-        <Footer />
+        <Footer /> */}
+        {/* 
+        <h4>{myname}</h4>
+        <h4>{friend}</h4>
+        <h4>{fsport}</h4> */}
+
+        {/* <h4> {myname}</h4>
+        <h4> {fdish}</h4> */}
+        {/* <h4>{merge}</h4> */}
+        {username}
+        {website}
       </div>
     )
   }
--- src/component/Header.js
+++ src/component/Header.js
@@ -4,22 +4,6 @@ import "./Header.css"
 export default function Header() {
     return (
         <div>
-            {/* <header>
-                <div className="container-fluid px-0">
-                    <div className="row no-gutters">
-                        <div className="col-lg-6">
-                            <a className="website-logo" href="#">My first React Website</a>
-                        </div>
-                        <div className="col-lg-6 navbar-links">
-                            <ul className="navbar-links-ul">
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Contact</a></li>
-                                <li><a href="#">About</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </header> */}
             <header>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <a className="navbar-brand" href="#">React JS</a>
