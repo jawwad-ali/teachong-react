edd8a4dd7663dd8d24fffe73c27b8514d1e320f9
jawwad-ali <alijawwad001@gmail.com>
2020-11-14 16:15:26 +0500

    forms in react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { useState } from 'react';
-import "./App.css"
-
-// REST OPERATORS
-// function add(num1, num2, ...num3) {
-//   // var ans = num1 + num2 + num3
-//   console.log(5, 10, 35, 5, 45, 95, 11, 22, 910)
-// }
 
 export default function App() {
 
-  const [defaultVal, setVal] = useState(0)
+  const [email, setemail] = useState("")
+  const [pass, setpass] = useState("")
+  const [gender , setgender] = useState()
+
+  const handleSubmit = () => {
+    alert(email)
+    alert(pass)
+    alert(gender)
+
+    setemail("")
+    setpass("")
+    setgender()
+  }
 
   return (
-    <div>
-      <h3>
-        {defaultVal}
-      </h3>
-      <button onClick={() => setVal(defaultVal + 1)}>
-        Inc
-      </button>
-      <button onClick={() => setVal(defaultVal-1)}>
-        Dec
-      </button>
+    <div className="container mt-5" >
+      <div className="text-center mt-2">
+        <h1>HANDLING FORMS WITH REACT</h1>
+      </div>
+
+      <div className="row">
+        <div className="col-lg-6 offset-lg-3">
+          <form onSubmit={handleSubmit}>
+            <input type="email" placeholder="Email" className="form-control mb-2" onChange={e => setemail(e.target.value)} />
+            <input type="password" className="form-control mb-2" placeholder="Password" onChange={e => setpass(e.target.value)} />
+            
+
+            <select onChange={e => setgender(e.target.value)}>
+              <option>Male</option>
+              <option>Female</option>
+            </select>
+            <br /> <br />
+
+            <input type="submit" value="Login" className="btn btn-success" />
+          </form>
+        </div>
+      </div>
+
     </div>
   )
 }
