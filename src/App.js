45680e1b94fd9620ebb9dcfc082fefe9f9953b6b
jawwad-ali <alijawwad001@gmail.com>
2020-11-26 16:00:46 +0500

    get multiple api data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import "./App.css"
 
 export default function App() {
 
   const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([])
 
   useEffect(() => {
     async function ApiCall() {
       setLoading(true)
 
-      const api = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+      const api = await fetch("https://jsonplaceholder.typicode.com/users")
 
       const response = await api.json()
 
       console.log(response)
+      setData(response)
       setLoading(false)
 
     }
     ApiCall()
-  } , [] )
+  }, [])
 
 
 
@@ -25,8 +28,17 @@ export default function App() {
     return <h4>Loading.....</h4>
 
   return (
-    <div>
-      <h4>Promises</h4>
+    <div className="data-container">
+      {
+        data.map((collect) => (
+          <div className="data">
+            <span><strong>Name</strong></span><p>{collect.name}</p>
+            <span><strong>Phone</strong></span><p>{collect.phone}</p>
+            <span><strong>Username</strong></span><p>{collect.username}</p>
+          </div>
+        ))
+      }
     </div>
+
   )
 }
