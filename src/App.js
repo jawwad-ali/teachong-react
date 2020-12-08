47c436c60fe7a85da5b37bfb40ad4da47d348b17
jawwad-ali <alijawwad001@gmail.com>
2020-12-08 15:20:09 +0500

    clean project

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import "./App.css"
+import React from 'react';
 
 export default function App() {
-
-  const [loading, setLoading] = useState(false)
-  const [name, setName] = useState([])
-  const [email, setEmail] = useState([])
-  const [street, setStreet] = useState([])
-
-
-  useEffect(() => {
-
-    async function ApiCall() {
-
-      setLoading(true)
-
-      const api = await fetch("https://jsonplaceholder.typicode.com/users/1")
-      const { name, email, address: { street } } = await api.json()
-      console.log(name)
-      console.log(email)
-      console.log(street)
-
-      setName(name)
-      setEmail(email)
-      setStreet(street)
-      // setData(email)
-
-      // const { address } = await api.json()
-
-      // console.log(address)
-      // setData(address)
-
-      setLoading(false)
-
-    }
-    ApiCall()
-  }, [])
-
-  if (loading)
-    return <h4>Loading.....</h4>
-
   return (
     <div className="data-container">
-      {name} <br />
-      {email} <br />
-      {street}  <br />
+      <h1>New Project</h1>
     </div>
 
   )
